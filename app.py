11dcac8d60f2d138762d47018f4d8a1c8f4129f9
bugrick <72883635+bugrick-dev@users.noreply.github.com>
2025-08-23 02:04:57 +0300

    Nearly finished layout.html and imported some important libraries

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_session import Session
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
