41fa0e1ea81a88dc3ff9e665bd2a745feaaf09f5
bugrick <72883635+bugrick-dev@users.noreply.github.com>
2025-08-25 15:24:35 +0300

    Added helpers.py with helper functions

---
+++ helpers.py
@@ -0,0 +1,14 @@
+from functools import wraps
+from flask import g, request, redirect, url_for, session
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session['username'] is None:
+            return redirect(url_for('login', next=request.url))
+        return f(*args, **kwargs)
+    return decorated_function
+
+def usd(value):
+    """Format value as USD."""
+    return f"${value:,.2f}"
